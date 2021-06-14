67af5c45436e58998e65ed44646dd27d8a35b2ef
quickstuff <85885288+quickstuff@users.noreply.github.com>
2021-06-14 16:19:14 +0100

    Add files via upload

---
+++ zeta_of_s.py
@@ -0,0 +1,28 @@
+from math import *
+
+cmd1 = 0
+cmd2 = 0
+eq = 1
+tm = 2
+
+def ask():
+    print("ζ(s)")
+    print("")
+    cmd1 = int(input("s = "))
+    cmd2 = int(input("Accuracy = "))
+    print("")
+    ans(eq,tm,cmd1,cmd2)
+
+def ans(eq,tm,cmd1,cmd2):
+    eq += 1/(tm**cmd1)
+    tm+=1
+
+    if tm >= cmd2:
+        print("ζ(",cmd1,") = ",eq)
+        print("________________________________")
+        print("")
+        ask()
+    else:
+        ans(eq,tm,cmd1,cmd2)
+
+ask()
